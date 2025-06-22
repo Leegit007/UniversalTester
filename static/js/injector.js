eb5768b0d7557d9f92d6380941478a8cebad94f9
Chandhini2 <muralee.ks@gmail.com>
2025-06-22 22:04:06 +0530

    Add files via upload

--- static/js/injector.js
+++ static/js/injector.js
@@ -1,1 +1,7 @@
+// Author & Designer: Muraleedharan K.S
+// Description: Placeholder for JavaScript DOM injection logic
+console.log("Injector.js loaded");
 
+// Example usage:
+// document.querySelector('#inputField').value = 'Test Value';
+// document.querySelector('#submitBtn').click();
